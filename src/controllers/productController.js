1e658b86c32273d28766d5ecdd62cd23f7930ef5
Jubayer Juhan <davidjuhan23@gmail.com>
2023-04-20 01:10:51 +0600

    Added file url parser to get a fileurl to save database

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,8 +1,16 @@
+import Product from "../models/productModel.js";
 import catchAsyncError from "../utils/catchAsyncError.js";
+import { fileUrlParser } from "../utils/files/fileUrlParser.js";
 
 // Controller Function To Add a Product To DB
 export const createProduct = catchAsyncError(async (req, res, next) => {
+  // parsing the file url to save database with the subfoldername
+  const fileUrl = fileUrlParser(req.file);
+
+  // storing the product on database
+  const product = await Product.create({ ...req.body, photo: fileUrl });
   res.status(200).json({
     success: true,
+    product,
   });
 });
