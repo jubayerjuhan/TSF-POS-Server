3778f39b7a8459ec7619ee0ea47ffbb0f79a0d21
Jubayer Juhan <davidjuhan23@gmail.com>
2023-05-05 01:44:38 +0600

    get and delete expense added

--- src/controllers/expenseController.js
+++ src/controllers/expenseController.js
@@ -1,3 +1,4 @@
+import ErrorHandler from "../middlewares/error/errorHandler.js";
 import Expense from "../models/expenseModel.js";
 import catchAsyncError from "../utils/catchAsyncError.js";
 
@@ -12,5 +13,36 @@ export const addExpense = catchAsyncError(async (req, res, next) => {
   res.status(200).json({
     success: true,
     message: "Expense added",
+  });
+});
+
+// controller function for deleting a expense
+export const deleteExpense = catchAsyncError(async (req, res, next) => {
+  const { id } = req.params;
+
+  const expense = await Expense.findByIdAndDelete(id);
+
+  // throwing error if no expense with the id available
+  if (!expense) return next(new ErrorHandler(400, "Expense not available"));
+
+  res.status(200).json({
+    success: true,
+    message: "Expense Deleted",
+  });
+});
+
+// controller function to get one expense document from database
+export const getExpense = catchAsyncError(async (req, res, next) => {
+  const { id } = req.params;
+
+  const expense = await Expense.findById(id).populate("branch", "name");
+
+  // throwing error if no expense with the id available
+  if (!expense)
+    return next(new ErrorHandler(404, "No expense found with this expense id"));
+
+  res.status(200).json({
+    success: true,
+    expense,
   });
 });
--- src/routes/expenseRoutes.js
+++ src/routes/expenseRoutes.js
@@ -2,12 +2,20 @@ import express from "express";
 import verifyAdminAndModerator from "../middlewares/verification/verifyAdminAndModerator.js";
 
 // import controller functions
-import { addExpense } from "../controllers/expenseController.js";
+import {
+  addExpense,
+  deleteExpense,
+  getExpense,
+} from "../controllers/expenseController.js";
 
 // Create a new router instance
 const router = express.Router();
 
 // Defineing all authentication routes here
 router.route("/add").post(verifyAdminAndModerator, addExpense);
+router
+  .route("/action/:id")
+  .get(verifyAdminAndModerator, getExpense)
+  .delete(verifyAdminAndModerator, deleteExpense);
 
 export default router;
