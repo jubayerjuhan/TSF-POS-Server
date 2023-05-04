f8f6536a1a0c0d48e01e30b19dceb757c42e8a6d
Jubayer Juhan <davidjuhan23@gmail.com>
2023-05-05 00:52:00 +0600

    add expense record to database

--- src/controllers/expenseController.js
+++ src/controllers/expenseController.js
@@ -1,10 +1,16 @@
 import Expense from "../models/expenseModel.js";
 import catchAsyncError from "../utils/catchAsyncError.js";
 
+// controller function to add expense
 export const addExpense = catchAsyncError(async (req, res, next) => {
-  // creating the branch
-  const expense = await Expense.create({ ...req.body });
+  // getting branch from req query
+  const { branch } = req.query;
+
+  // creating the expense document in database
+  await Expense.create({ ...req.body, branch });
+
   res.status(200).json({
     success: true,
+    message: "Expense added",
   });
 });
