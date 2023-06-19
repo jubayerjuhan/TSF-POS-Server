07da9bfe7bb6076d8518d3d848d0a33916ee5758
Jubayer Juhan <davidjuhan23@gmail.com>
2023-06-19 16:26:11 +0600

    added custom order model

---
+++ src/models/customOrder.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const customOrderSchema = new mongoose.Schema(
+  {
+    customerName: { type: String, required: true },
+    customerPhone: { type: Number, required: true },
+    description: { type: String, required: true },
+    totalPrice: { type: Number, required: true },
+    status: {
+      type: String,
+      enum: ["Order Taken", "Shipped", "Delivered"],
+      default: "Order Taken",
+    },
+    advancePayment: { type: Number, default: 0, required: true },
+    branch: { type: String, required: true },
+    products: [
+      {
+        product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+        quantity: { type: Number, required: true },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const CustomOrder = mongoose.model("CustomOrder", customOrderSchema);
+
+export default CustomOrder;
