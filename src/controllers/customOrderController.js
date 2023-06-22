50b5fb9fbafa2555ec850fa8735011dfd34759b4
Jubayer Juhan <davidjuhan23@gmail.com>
2023-06-23 00:23:22 +0600

    Get Custom Order By Id Products Populated and On Updated Order Too

--- src/controllers/customOrderController.js
+++ src/controllers/customOrderController.js
@@ -42,7 +42,7 @@ export const getCustomOrderById = catchAsyncError(async (req, res, next) => {
 
   const order = await CustomOrder.findById(id)
     .populate("branch", "name") // Populating the branch field with the 'name' field
-    .populate("products.product", "name"); // Populating the 'product' field within the 'products' array with the 'name' field
+    .populate("products.product"); // Populating the 'product' field within the 'products' array with the 'name' field
 
   if (!order) {
     return next(new ErrorHandler(404, "Custom Order not found"));
@@ -138,10 +138,14 @@ export const updateCustomOrderStatus = catchAsyncError(
 
     const updatedOrder = await order.save();
 
+    const orderToDispatch = await CustomOrder.findById(updatedOrder._id)
+      .populate("branch", "name") // Populating the branch field with the 'name' field
+      .populate("products.product"); // Populating the 'product' field within the 'products' array with the 'name' field
+
     res.status(200).json({
       success: true,
       message: "Order Status Updated",
-      order: updatedOrder,
+      order: orderToDispatch,
     });
   }
 );
